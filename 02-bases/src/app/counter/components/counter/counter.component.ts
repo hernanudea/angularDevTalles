ae5a2f1aa959383aa9eee7c8d212b081d959234e
Hernan Velasquez <hernanudea@gmail.com>
2024-01-14 00:45:45 -0500

    Bases de Angular, incluye componentes y modulos

---
+++ 02-bases/src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <p>Counter = {{ counter }}</p>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+    <hr>
+  `
+})
+
+export class CounterComponent {
+
+  public counter: number = 10;
+
+  constructor() { }
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
+
+
+}
