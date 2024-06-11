e30c300ad310b60d1df3d1e898b0aa3d397b75e2
Hernan Velasquez <hernanudea@gmail.com>
2024-06-10 22:14:47 -0500

    Se agrega Se agregan las rutas hijas con lazyLoad

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutPagesComponent} from "./shared/pages/about-pages/about-pages.component";
+import {ContactPageComponent} from "./shared/pages/contact-page/contact-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+/*  {path: 'home', component: HomePageComponent},*/
+  {path: 'about', component: AboutPagesComponent},
+  {path: 'contact', component: ContactPageComponent},
+  {path: 'countries', loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule)},
+  {path: '**', redirectTo: 'countries'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {AboutPagesComponent} from './pages/about-pages/about-pages.component';
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {RouterModule} from "@angular/router";
+import {ContactPageComponent} from './pages/contact-page/contact-page.component';
+import {SearchBoxComponent} from './components/search-box/search-box.component';
+
+
+@NgModule({
+  declarations: [
+    AboutPagesComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    SearchBoxComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    AboutPagesComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    SearchBoxComponent
+
+  ]
+})
+export class SharedModule {
+}
