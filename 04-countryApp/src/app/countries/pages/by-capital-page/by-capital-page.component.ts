7fc3e3ca8aaab31123ede89d3d6472ca07846f4f
Hernan Velasquez <hernanudea@gmail.com>
2024-06-13 22:28:08 -0500

    Se crea un event emiter, permite lanzar un evento y captarlo desde otro componente

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  searchByCapital(term: string) {
+    console.log("Desde ByCapital Page");
+    console.log(term);
+  }
 }
